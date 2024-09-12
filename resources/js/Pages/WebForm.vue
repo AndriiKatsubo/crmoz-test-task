d34f47b7674d6bd97ee8cf09613c6e9ea1930c1c
Andrii Katsubo <andrii.katsubo@gmail.com>
2024-09-12 11:01:21 +0300

    Removed layout for webform

--- resources/js/Pages/WebForm.vue
+++ resources/js/Pages/WebForm.vue
@@ -1,81 +1,86 @@
 <script setup>
-import WebFormLayout from '@/Layouts/WebFormLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
+    <div>
+        <div class="min-h-screen bg-gray-100">
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <h1 class="font-semibold text-xl text-gray-800 leading-tight">Web Form</h1>
+                </div>
+            </header>
 
-    <Head title="Web Form" />
+            <!-- Page Content -->
+            <main>
+                <Head title="Web Form" />
+                <form @submit.prevent="submitForm">
+                    <div class="space-y-12 mx-6 mt-6 max-w-4xl mx-auto">
+                        <div class="border-b border-gray-900/10 pb-12">
+                            <h2 class="text-base font-semibold leading-7 text-gray-900">Deal record</h2>
+                            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div class="sm:col-span-6">
+                                    <label for="deal-name"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="deal-name" id="deal-name" v-model="dealName"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    </div>
+                                </div>
 
-    <WebFormLayout>
-        <template #header>
-            <h1 class="font-semibold text-xl text-gray-800 leading-tight">Web Form</h1>
-        </template>
-
-        <form @submit.prevent="submitForm">
-            <div class="space-y-12 mx-6 mt-6 max-w-4xl mx-auto">
-                <div class="border-b border-gray-900/10 pb-12">
-                    <h2 class="text-base font-semibold leading-7 text-gray-900">Deal record</h2>
-                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div class="sm:col-span-6">
-                            <label for="deal-name"
-                                class="block text-sm font-medium leading-6 text-gray-900">Name</label>
-                            <div class="mt-2">
-                                <input type="text" name="deal-name" id="deal-name" v-model="dealName"
-                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                <div class="sm:col-span-6">
+                                    <label for="deal-stage"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Stage</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="deal-stage" id="deal-stage" v-model="dealStage"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
-                        <div class="sm:col-span-6">
-                            <label for="deal-stage"
-                                class="block text-sm font-medium leading-6 text-gray-900">Stage</label>
-                            <div class="mt-2">
-                                <input type="text" name="deal-stage" id="deal-stage" v-model="dealStage"
-                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        <div class="border-b border-gray-900/10 pb-12">
+                            <h2 class="text-base font-semibold leading-7 text-gray-900">Account record</h2>
+                            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div class="sm:col-span-6">
+                                    <label for="account-name"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="account-name" id="account-name" v-model="accountName"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-6">
+                                    <label for="account-website"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Website</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="account-website" id="account-website" v-model="accountWebsite"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-6">
+                                    <label for="account-phone"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Phone</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="account-phone" id="account-phone" v-model="accountPhone"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    </div>
+                                </div>
                             </div>
+                        </div>
+
+                        <div class="my-6 flex items-center justify-end gap-x-6">
+                            <button type="submit"
+                                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
                         </div>
                     </div>
-                </div>
-
-                <div class="border-b border-gray-900/10 pb-12">
-                    <h2 class="text-base font-semibold leading-7 text-gray-900">Account record</h2>
-                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div class="sm:col-span-6">
-                            <label for="account-name"
-                                class="block text-sm font-medium leading-6 text-gray-900">Name</label>
-                            <div class="mt-2">
-                                <input type="text" name="account-name" id="account-name" v-model="accountName"
-                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                            </div>
-                        </div>
-
-                        <div class="sm:col-span-6">
-                            <label for="account-website"
-                                class="block text-sm font-medium leading-6 text-gray-900">Website</label>
-                            <div class="mt-2">
-                                <input type="text" name="account-website" id="account-website" v-model="accountWebsite"
-                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                            </div>
-                        </div>
-
-                        <div class="sm:col-span-6">
-                            <label for="account-phone"
-                                class="block text-sm font-medium leading-6 text-gray-900">Phone</label>
-                            <div class="mt-2">
-                                <input type="text" name="account-phone" id="account-phone" v-model="accountPhone"
-                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="my-6 flex items-center justify-end gap-x-6">
-                    <button type="submit"
-                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-                </div>
-            </div>
-        </form>
-    </WebFormLayout>
+                </form>
+            </main>
+        </div>
+    </div>
 </template>
 
 <script>
